1087d68dc8663a696e6a840405d8ad5915d4a29a
imafukuyukinari <49267983+imafukuyukinari@users.noreply.github.com>
2020-11-28 18:12:34 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,33 @@
 <?php
-echo 'Hello World!';
+
+require_once('./../../controller/TodoController.php');
+
+$controller = new TodoController;
+$todo_list = Todo::findAll();
+
 
 ?>
+
+<!DOCTYPEhtml>
+<htmllang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>TODOリスト</title>
+</head>
+<body>
+    <div><a href="./new.php">新規作成</a></div>
+    <?php if($todo_list):?>
+    <ul>
+        <?php foreach($todo_list as $todo):?>
+            <li><a href="./detail.php?todo_id=<?php echo $todo['id'] ?>"><?php echo $todo['title'];?></a></li>
+        <?php endforeach;?>
+    </ul>
+    </div>
+    <?php else:?>
+        <div>データなし</div>
+    <?php endif;?>
+</body>
+</html>
+
