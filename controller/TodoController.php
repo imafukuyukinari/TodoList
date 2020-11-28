292d6d516a1078f7b1865f7b2d388bd3e2fb51bd
imafukuyukinari <yukinari302@gmail.com>
2020-11-28 18:09:04 +0900

    first commit

---
+++ controller/TodoController.php
@@ -0,0 +1,27 @@
+<?php
+require_once('./../../model/Todo.php');
+
+class TodoController{
+    public function index(){
+        $todo_list = Todo::findAll();
+        return $todo_list;
+    }
+
+    public function detail(){
+        $todo_id = $_GET['todo_id'];
+
+        $todo_list = Todo::findById($todo_id);
+       
+        return $todo_list;
+    }
+    public function new(){
+        $title = $_POST['title'];
+        $detail = $_POST['detail'];
+
+        $todo = new Todo;
+        $todo->setTitle($title);
+        $todo->setDetail($detail);
+        $todo->save();
+    }
+
+}
