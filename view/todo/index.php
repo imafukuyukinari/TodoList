2387eec0a41ed0eb5f2c9f9850a975f21951fb56
imafukuyukinari <49267983+imafukuyukinari@users.noreply.github.com>
2020-12-20 19:01:39 +0900

    Update index.php

--- view/todo/index.php
+++ view/todo/index.php
@@ -1,9 +1,12 @@
 <?php
 require_once('./../../controller/TodoController.php');
 
+// $controller = new TodoController;
+// $todo_list = $controller->index();
 
-$controller = new TodoController;
-$todo_list = $controller->index();
+session_start();
+$error_msgs = $_SESSION['error_msgs'];
+unset($_SESSION['error_msgs']);
 
 ?>
 
@@ -18,11 +21,44 @@ $todo_list = $controller->index();
     <?php if($todo_list):?>
     <ul>
         <?php foreach($todo_list as $todo):?>
-        <li><a href="./detail.php?todo_id=<?php echo $todo['id'] ?>"><?php echo $todo['title'];?></a> : <?php echo $todo['display_status'];?></li>
+        <li><a href="./detail.php?todo_id=<?php echo $todo['id'] ?>"><?php echo $todo['title'];?></a> : <?php echo $todo['display_status'];?> <button class="delete-btn">削除</button></li>
         <?php endforeach;?>
     </ul>
         <?php else: ?>
             <p>データなし</p>
         <?php endif; ?>
+
+        <?php if($error_msgs):?>
+         <div>
+            <ul>
+                <?php foreach((array)$error_msgs as $error_msg):?>
+                    <li><?php echo $error_msg;?></li>
+                <?php endforeach;?>
+            </ul>
+        </div>
+    <?endif;?>
+    <script src="./../../public/js/jquery-3.5.1.min.js"></script>
+    <script>
+    $(".delete-btn").click(function() {
+        let data = {};
+        data.todo_id = 1;
+
+        $.ajax({
+            url: './delete.php',
+            type: 'post',
+            data: data
+        }).then(
+            function(data){
+                let json = JSON.parse(data);
+                console.log('success', json);
+            },
+            function(){
+                console.log('fail');
+                alert('fail');
+            }
+        )
+    });
+    
+    </script>
 </body>
 </html>
