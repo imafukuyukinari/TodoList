a3afcefc42cf66cfc9abb935f796dd60f676bf64
imafukuyukinari <49267983+imafukuyukinari@users.noreply.github.com>
2020-12-20 18:44:48 +0900

    Update Todo.php

--- model/Todo.php
+++ model/Todo.php
@@ -8,9 +8,19 @@ class Todo{
     const STATUS_INCOMPLETE_TXT = '未完了';
     const STATUS_COMPLETE_TXT = '完了';
 
+    public $id;
     public $title;
     public $detail;
     public $status;
+
+    public function getId(){
+        return $this->id;
+    }
+
+    public function setId($id){
+        $this->id = $id;
+    }
+
 
     public function getTitle(){
         return $this->title;
@@ -86,8 +96,6 @@ class Todo{
             $todo['display_status'] = self::getDisplayStatus($todo['status']);
         }
 
-
-
         return $todo;
     }
 
@@ -102,19 +110,107 @@ class Todo{
     }
     
     public function save(){
-        $query = sprintf("INSERT INTO `todos` (`title`, `detail`, `status`,
-                    `created_at`, `updated_at`)
-                VALUES ('%s', '%s', 0, NOW(), NOW())",
-                    $this->title,
-                    $this->detail
-                );
+        try{
+            throw new Exception('エラー!!!!!!!');
+            $query = sprintf("INSERT INTO `todos` (`title`, `detail`, `status`,
+                `created_at`, `updated_at`)
+            VALUES ('%s', '%s', 0, NOW(), NOW())",
+                $this->title,
+                $this->detail
+            );
 
         $pdo = new PDO(DSN, USERNAME, PASSWORD);
+
+        $pdo->beginTransaction();
+
         $result = $pdo->query($query);
 
+        $pdo->commit();
+
+        }catch(Exception $e){
+            error_log('新規作成に失敗しました。');
+            error_log($e->getMessage());
+            error_log($e->getTraceAsString());
+
+            return false;
+        }
+
+
         return $result;
-
-
     }
+
+    public function update(){
+        try{
+            $query = sprintf("UPDATE `todos` SET `title` = '%s', `detail` = '%s', `updated_at` = '%s' WHERE id = %s",
+                $this->title,
+                $this->detail,
+                date('Y-m-d H:i:s'),
+                $this->id
+            );
+
+        $pdo = new PDO(DSN, USERNAME, PASSWORD);
+
+        $pdo->beginTransaction();
+
+        $result = $pdo->query($query);
+
+        $pdo->commit();
+
+        }catch(Exception $e){
+            error_log('更新に失敗しました。');
+            error_log($e->getMessage());
+            error_log($e->getTraceAsString());
+
+            $pdo->rollBack();
+
+            return false;
+        }
+
+
+        return $result;
+    }
+
+
+    public static function isExistById($todo_id){
+        $pdo = new PDO(DSN, USERNAME, PASSWORD);
+        $stmh = $pdo->query(sprintf('select * from todos where id = %s;', $todo_id));
+        if($stmh){
+            $todo = $stmh->fetch(PDO::FETCH_ASSOC);
+        }else{
+            $todo = array();
+        }   
+
+        if($todo){
+            return true;
+        }
+        return false;
+    }
+
+    public function delete(){
+        try{
+            $query = sprintf("DELETE FROM todos WHERE id = %s",
+                $this->id
+            );
+
+        $pdo = new PDO(DSN, USERNAME, PASSWORD);
+
+        $pdo->beginTransaction();
+
+        $result = $pdo->query($query);
+
+        $pdo->commit();
+
+        }catch(Exception $e){
+            error_log('削除に失敗しました。');
+            error_log($e->getMessage());
+            error_log($e->getTraceAsString());
+
+            $pdo->rollBack();
+
+            return false;
+        }
+
+        return $result;
+    }
+
 }
- 
