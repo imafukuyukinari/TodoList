8d0e4aa4b23d6fd14e6cec9e105467ca76871715
imafukuyukinari <49267983+imafukuyukinari@users.noreply.github.com>
2020-12-20 18:43:52 +0900

    Update TodoController.php

--- controller/TodoController.php
+++ controller/TodoController.php
@@ -11,6 +11,16 @@ class TodoController{
 
     public function detail(){
         $todo_id = $_GET['todo_id'];
+        if(!$todo_id){
+            header('Location: ./../error/404.php');
+            return;
+        }
+
+        if(Todo::isExistById($todo_id) === false){
+            header('Location: ./../error/404.php');
+            return;
+        }
+
         $todo = Todo::findById($todo_id);
         return $todo;
     }
@@ -25,30 +35,139 @@ class TodoController{
         $validation->setData($data);
         if($validation->check() === false){
             $error_msgs = $validation->getErrorMessages();
-            
+
             session_start();
             $_SESSION['error_msgs'] = $error_msgs;
 
             $params = sprintf('?title=%s&detail=%s', $_POST['title'], $_POST['detail']);
             header(sprintf('Location: ./new.php%s', $params));
+            return;
         }
 
         $valid_data = $validation->getData();
-        var_dump($valid_data);
 
 
         $todo = new Todo;
-        $todo->setTitle($title);
-        $todo->setDetail($detail);
+        $todo->setTitle($valid_data['title']);
+        $todo->setDetail($valid_data['detail']);
         $result = $todo->save();
 
-        $result = false;
         if($result === false){
-            $params = sprintf('?title=%s&detail=%s', $_POST['title'], $_POST['detail']);
+            $params = sprintf('?title=%s&detail=%s', $valid_data['title'], $valid_data['detail']);
             header(sprintf('Location: ./new.php%s', $params));
             return;
         }
 
         header('Location: ./index.php');
+    }
+
+    public function edit(){
+        $todo_id = '';
+        $params = array();
+        if($_SERVER['REQUEST_METHOD'] === 'GET'){
+            if(isset($_GET['todo_id'])){
+                $todo_id = $_GET['todo_id'];
+            }
+            if(isset($_GET['title'])){
+                $params['title'] = $_GET['title'];
+            }
+            if(isset($_GET['detail'])){
+                $params['detail'] = $_GET['detail'];
+            }
+        }
+
+
+        if(!$todo_id){
+            header('Location: ./../error/404.php');
+            return;
+        }
+
+        if(Todo::isExistById($todo_id) === false){
+            header('Location: ./../error/404.php');
+            return;
+        }
+
+        $todo =Todo::findbyId($todo_id);
+        if(!$todo){
+            header('Location: ./../error/404.php');
+            return;
+        }
+
+
+        $data = array(
+            'todo' => $todo,
+            'params' => $params,
+        );
+        return $data;
+    }
+    
+    public function update(){
+        if(!$_POST['todo_id']){
+            session_start();
+            $_SESSION['error_msgs'] = '指定したIDに該当するデータがありません。';
+            header('Location: ./index.php');
+            return;
+        }
+        if(Todo::isExistById($_POST['todo_id']) === false){
+            $params = sprintf('?todo_id=%stitle=%s&detail=%s', $_POST['todo_id'], $_POST['title'], $_POST['detail']);
+            header(sprintf('Location: ./edit.php%s', $params));
+            return;
+        }
+
+        $data = array(
+            'todo_id' => $_POST['todo_id'],
+            'title' => $_POST['title'],
+            'detail'=> $_POST['detail'],
+        );
+
+
+        $validation = new Todovalidation;
+        $validation->setData($data);
+        if($validation->check() === false){
+            $error_msgs = $validation->getErrorMessages();
+
+            session_start();
+            $_SESSION['error_msgs'] = $error_msgs;
+
+            $params = sprintf('?todo_id=%s&title=%s&detail=%s', $_POST['todo_id'], $_POST['title'], $_POST['detail']);
+            header(sprintf('Location: ./edit.php%s', $params));
+            return;
+        }
+
+        $valid_data = $validation->getData();
+
+
+        $todo = new Todo;
+        $todo->setId($valid_data['todo_id']);
+        $todo->setTitle($valid_data['title']);
+        $todo->setDetail($valid_data['detail']);
+        $result = $todo->update();
+
+        if($result === false){
+            $params = sprintf('?title=%s&detail=%s', $valid_data['title'], $valid_data['detail']);
+            header(sprintf('Location: ./edit.php%s', $params));
+            return;
+        }
+
+        header('Location: ./index.php');
+    }
+
+    public function delete(){
+        $todo_id = $_POST['todo_id'];
+        if(!$todo_id){
+            error_log(sprintf("[TodoController][delete]todo_id not found. todo_id: %s", $todo_id));
+            return false;
+        }
+
+        if(Todo::isExistById($todo_id) === false){
+            error_log(sprintf("[TodoController][delete]record is not found. todo_id: %s", $todo_id));
+            return false;
+        }
+
+        $todo = new Todo;
+        $todo->setId($todo_id);
+        $result = $todo->delete();
+
+        return $result;
     }
 }
