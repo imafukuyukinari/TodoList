2075eb42307cefaea98238ae7c992c3530785568
imafukuyukinari <49267983+imafukuyukinari@users.noreply.github.com>
2020-12-28 17:12:46 +0900

    Update detail.php

--- view/todo/detail.php
+++ view/todo/detail.php
@@ -1,32 +1,38 @@
 <?php
-require_once('./../../controller/TodoController.php');
+require_once '../../config/database.php';
+require_once '../../model/todo.php';
+require_once '../../controller/TodoController.php';
 
-$controller = new TodoController;
-$todo = $controller->detail();
-
+$action = new TodoController;
+$todo = $action->detail();
 ?>
-
-
-<html lang="en">
+<!DOCTYPE html>
+<html lang="ja">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>詳細画面</title>
 </head>
 <body>
-    <h1>詳細画面</h1>
-<div>
-    <div>タイトル</div>
-    <div><?php echo $todo['title']; ?></div>
-</div>
-<div>
-    <div>詳細</div>
-    <div><?php echo $todo['detail']; ?></div>
-</div>
-<div>
-    <div>ステータス</div>
-    <div><?php echo $todo['display_status']; ?></div>
-</div>
-
+    <table class="table">
+        <thead>
+            <tr>
+                <th>タイトル</th>
+                <th>詳細</th>
+                <th>締め切り</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td scope="row"><?php echo $todo['title']; ?></td>
+                <td><?php echo $todo['detail']; ?></td>
+                <td><?php echo $todo['deadline_date']; ?></td>
+            </tr>
+        </tbody>
+    </table>
+    <div>
+        <button><a href="./edit.php">編集</a></button>
+    </div>
 </body>
 </html>
