546f433a25bb5261c3cdc733cae281ea2e422cb0
imafukuyukinari <49267983+imafukuyukinari@users.noreply.github.com>
2020-12-28 17:13:14 +0900

    Update edit.php

--- view/todo/edit.php
+++ view/todo/edit.php
@@ -1,54 +1,46 @@
 <?php
-require_once('./../../controller/TodoController.php');
-
-if($_SERVER['REQUEST_METHOD'] === 'POST'){
-    $controller = new TodoController();
-    $controller->update();
-    exit;
-}
-
-$controller = new TodoController();
-$data = $controller->edit();
-$todo = $data['todo'];
-$params = $data['params'];
-
+require_once '../../config/database.php';
+require_once '../../model/todo.php';
+require_once '../../controller/TodoController.php';
 
 session_start();
+//セッション情報の取得
 $error_msgs = $_SESSION['error_msgs'];
-unset($_SESSION['error_msgs']);
-
+//セッション削除
+unset($_SESSION["error_msgs"]);
 
 ?>
-
-
-<html lang="en">
+<!DOCTYPE html>
+<html lang="ja">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>編集</title>
 </head>
 <body>
-    <h1>編集</h1>
-    <form action='./edit.php' method='post'>
+    <div>編集</div>
+    <form action="./new.php" method="post">
         <div>
             <div>タイトル</div>
-<input name='title' type='text' value="<?php if(isset($params['title']) ):?><?php echo $params['title'];?><?php else:?><?php echo $todo['title'];?><?php endif;?>">
+            <div>
+                <input name="title" type="text" value="<?php echo $todo['title'];?>">
+            </div>
         </div>
         <div>
             <div>詳細</div>
-            <textarea name='detail'><?php if(isset($params['title']) ):?><?php echo $params['detail'];?><?php else:?><?php echo $todo['detail'];?><?php endif;?></textarea>
+            <div>
+                <textarea name="detail">"<?php echo $todo['detail'];?></textarea>
+            </div>
         </div>
-        <input type="hidden" name='todo_id' value="<?php echo $todo['id'];?>">
-        <button type='submit'>更新</button>
+        <button type="submit">登録</button>
     </form>
     <?php if($error_msgs):?>
-         <div>
+        <div>
             <ul>
-                <?php foreach($error_msgs as $error_msg):?>
-                    <li><?php echo $error_msg;?></li>
-                <?php endforeach;?>
+            <?php foreach($error_msgs as $error_msg):?>
+                <li><?php echo $error_msg; ?></li>
+            <?php endforeach;?>
             </ul>
         </div>
-    <?endif;?>
+    <?php endif;?>
 </body>
 </html>
