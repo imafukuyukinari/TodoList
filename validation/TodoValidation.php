9bb7f9fce18ef750ffceeb80bc1eb1bfa6ed26dc
imafukuyukinari <49267983+imafukuyukinari@users.noreply.github.com>
2020-12-28 17:11:37 +0900

    Update TodoValidation.php

--- validation/TodoValidation.php
+++ validation/TodoValidation.php
@@ -1,33 +1,34 @@
 <?php
-class TodoValidation{
-    public $data = array();
-    public $error_msgs = array();
+class TodoValidation {
+    private $data;
+    private $error_msgs = array();
 
-    public function setData($data){
+    public function setData($data) {
         $this->data = $data;
     }
 
-    public function getData($data){
+    public function getData() {
         return $this->data;
     }
 
-    public function getErrorMessages(){
+    public function getErrorMessages() {
         return $this->error_msgs;
     }
 
-    public function check(){
-        if(isset($this->data['title']) && empty($this->$data['title'])){
-            $this->error_msgs[] = 'タイトルが空です';
+    public function check() {
+        $title = $this->data['title'];
+        $detail = $this->data['detail'];
+
+        if(empty($title)) {
+            $this->error_msgs[] = 'タイトルが空です。';
         }
-        if(isset($this->data['detail']) && empty($this->$data['detail'])){
-            $this->error_msgs[] = '詳細が空です';
+        if(empty($detail)) {
+            $this->error_msgs[] = '詳細が空です。';
         }
 
-        if(count($this->error_msgs) > 0){
-           return false;
+        if(count($this->error_msgs) > 0) {
+            return false;
         }
         return true;
     }
-
 }
-
