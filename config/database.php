2b066fc2b489b6f23e14ef661dc250861cff2d2f
imafukuyukinari <49267983+imafukuyukinari@users.noreply.github.com>
2020-12-28 17:09:52 +0900

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,5 +1,5 @@
 <?php
 
-const DSN ='mysql:host=mysql;dbname=COMMON;charset=utf8';
+const DSN ='mysql:host=071b91442f8c;dbname=COMMON;charset=utf8';
 const USERNAME = 'yukinari';
 const PASSWORD = 'yukinari';
